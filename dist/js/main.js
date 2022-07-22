a842089b330c62577835920e3e1f059bc7eb9008
Roman Lukjanets <lukyanets14@gmail.com>
2022-07-22 14:43:21 +0300

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,69 @@
+
+const body = document.querySelector('body'),
+    html = document.querySelector('html'),
+    menu = document.querySelectorAll('._burger, .header__nav, body'),
+    burger = document.querySelector('._burger'),
+    header = document.querySelector('.header');
+
+
+
+let thisTarget;
+body.addEventListener('click', function (event) {
+
+    thisTarget = event.target;
+
+    // Меню в шапке
+    if (thisTarget.closest('._burger')) {
+        menu.forEach(elem => {
+            elem.classList.toggle('_active')
+        })
+    }
+
+
+})
+
+
+// =-=-=-=-=-=-=-=-=-=-=-=- <slider> -=-=-=-=-=-=-=-=-=-=-=-=
+/*
+let slider = new Swiper('.__slider', {
+  
+    spaceBetween: 30,
+    slidesPerView: 1,
+    centeredSlides: false,
+
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      992: {
+        slidesPerView: 3,
+        centeredSlides: true,
+    
+      },
+      600: {
+        slidesPerView: 2,
+        centeredSlides: false,
+      },
+    }
+}); 
+*/
+// =-=-=-=-=-=-=-=-=-=-=-=- </slider> -=-=-=-=-=-=-=-=-=-=-=-=
+
+
+/* 
+// =-=-=-=-=-=-=-=-=-=-=-=- <Анимации> -=-=-=-=-=-=-=-=-=-=-=-=
+
+wow = new WOW({
+mobile:       false,
+})
+wow.init();
+
+// =-=-=-=-=-=-=-=-=-=-=-=- </Анимации> -=-=-=-=-=-=-=-=-=-=-=-=
+
+*/
